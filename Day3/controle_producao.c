361acc8f8c86136f7faf1f4b245f274a7eb0d50c
guioliveiraxd <guioliveira1177@gmail.com>
2026-06-13 00:21:03 -0300

    Exercise completed.

--- Day3/controle_producao.c
+++ Day3/controle_producao.c
@@ -1,23 +1,10 @@
-/*Desafio #day3 - Controle de Produção
-Problema
-Uma fábrica deseja acompanhar a produção diária de peças durante um período.
-Seu programa deve ler a quantidade de dias monitorados e, em seguida, a quantidade de peças produzidas em cada dia.
-Ao final, exiba:
-O total de peças produzidas.
-A média de produção por dia.
-O maior valor de produção registrado.
-Entrada
-A primeira linha contém um número inteiro N (1 ≤ N ≤ 30), representando a quantidade de dias monitorados.
-Nas próximas N linhas haverá um número inteiro P, representando a quantidade de peças produzidas naquele dia.
-Saída
-Seu programa deve exibir exatamente no formato:
-Total: X
-Media: Y
-Maior: Z
-Onde:
-X é o total produzido.
-Y é a média com duas casas decimais.
-Z é a maior produção registrada.*/
+/*Desafio #day3 - Controle de Produção Problema Uma fábrica deseja acompanhar a produção diária de peças durante
+ um período. Seu programa deve ler a quantidade de dias monitorados e, em seguida, a quantidade de peças produzidas
+ em cada dia. Ao final, exiba: O total de peças produzidas. A média de produção por dia. O maior valor de produção
+ registrado. Entrada A primeira linha contém um número inteiro N (1 ≤ N ≤ 30), representando a quantidade de dias monitorados.
+ Nas próximas N linhas haverá um número inteiro P, representando a quantidade de peças produzidas naquele dia.
+ Saída Seu programa deve exibir exatamente no formato: Total: X Media: Y Maior: Z Onde: X é o total produzido. Y é a média
+ com duas casas decimais. Z é a maior produção registrada.*/
 
 #include <stdio.h>
 
@@ -27,12 +14,20 @@ int main() {
     int total = 0;
     double media;
 
-    printf("Quantos dia foram monitorados: \n");
     scanf("%d", &diasMonitorados);
 
-    
+    for (int dia = 1; dia <= diasMonitorados; dia++) {
+        scanf("%d", &pecasProduzidas);
+        total += pecasProduzidas;
+        if (pecasProduzidas > maiorProducao) {
+            maiorProducao = pecasProduzidas;
+        }
+    }
+    media = (double) total / diasMonitorados;
 
+    printf("Total: %d\n", total);
+    printf("Media: %.2lf\n", media);
+    printf("Maior: %d\n", maiorProducao);
 
-
-
+    return 0;
 }
