9af1f5c54a7ec98652a186436d6fd96b0393c0b5
guioliveiraxd <guioliveira1177@gmail.com>
2026-06-14 20:13:44 -0300

    dois exercicios + configurando cmake

--- Day5/exerc1.c
+++ Day5/exerc1.c
@@ -1,7 +1,11 @@
 /*
  Exercicios para fixar loop "For"
-
 1. Contagem simples
-
 Problema: Exiba os números de 1 a 10 usando for.
  */
+#include <stdio.h>
+ int main(){
+ for (int i = 1; i < 11; i++) {
+ printf("%d\n", i);
+ }
+ }
---
+++ Day5/exerc2.c
@@ -0,0 +1,10 @@
+// Desafio #Day5.2 - Contagem Regressiva
+//
+// Problema:
+// Exiba os números de 10 até 1 usando um laço FOR.
+#include <stdio.h>
+ int main(){
+     for (int i = 10; i >0 ; i--) {
+         printf("%d\n", i);
+     }
+ }
