fda8d0a905b1f294c715a5b7d483c15f327b9c7e
guioliveiraxd <guioliveira1177@gmail.com>
2026-06-16 20:53:13 -0300

    day7

--- Day7/day7.c
+++ Day7/day7.c
@@ -2,7 +2,22 @@
 //
 //
 #include <stdio.h>
+
 int main() {
+    char resp;
+    double c, f;
 
+    do {
+        printf("Digite a temperatura em celsius:\n");
+        scanf("%lf", &c);
 
+        f = 9.0 * c / 5.0 + 32.0;
+
+        printf("Equivalente em Fahrenheit: %.1lf\n", f);
+
+        printf("Deseja repetir (s/n)? ");
+        scanf(" %c", &resp);
+
+    } while (resp == 's' || resp == 'S');
+    return 0;
 }
