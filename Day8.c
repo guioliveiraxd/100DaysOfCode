4181064817ba6f149642c498cb27842ae1acc0b5
Guilherme Oliveira <164787576+guioliveiraxd@users.noreply.github.com>
2026-06-17 22:06:21 -0300

    Criando desafio day8

---
+++ Day8.c
@@ -0,0 +1,43 @@
+/*
+
+Desafio - Caixa Eletrônico
+
+Crie um programa que simule um caixa eletrônico.
+
+O programa deve:
+
+1. Pedir uma senha ao usuário.
+
+2. A senha correta é 1234.
+
+3. Enquanto a senha estiver errada, pedir novamente.
+
+4. Quando a senha estiver correta, exibir:
+
+   "Acesso liberado!"
+
+Obrigatório:
+
+- Utilizar a estrutura do...while.
+
+- Contar quantas tentativas foram necessárias.
+
+- Exibir a quantidade de tentativas ao final.
+
+Exemplo:
+
+Digite a senha: 1111
+
+Senha incorreta!
+
+Digite a senha: 2222
+
+Senha incorreta!
+
+Digite a senha: 1234
+
+Acesso liberado!
+
+Tentativas: 3
+
+*/
