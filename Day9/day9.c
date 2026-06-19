abf19675ab5c0dce594e736453756c447c0b0bc9
guioliveiraxd <guioliveira1177@gmail.com>
2026-06-18 23:05:53 -0300

    day9

--- Day9/day9.c
+++ Day9/day9.c
@@ -1,3 +1,28 @@
-//
-// Created by guiol on 18/06/2026.
-//
+/*
+Desafio - Número Secreto
+Crie um programa que simule um jogo de adivinhação.
+O programa deve:
+1. Definir um número secreto (50).
+2. Pedir um número ao usuário.
+3. Enquanto o usuário não acertar, continuar pedindo novos números.
+4. Se o número digitado for maior que o número secreto, exibir:
+   "Muito alto!"
+5. Se o número digitado for menor que o número secreto, exibir:
+   "Muito baixo!"
+6. Quando o usuário acertar, exibir:
+   "Parabéns, você acertou!"
+Obrigatório:
+- Utilizar a estrutura do...while.
+- Contar quantas tentativas foram necessárias.
+- Exibir a quantidade de tentativas ao final.
+Exemplo:
+Digite um numero: 30
+Muito baixo!
+Digite um numero: 70
+Muito alto!
+Digite um numero: 45
+Muito baixo!
+Digite um numero: 50
+Parabéns, você acertou!
+Tentativas: 4
+*/
