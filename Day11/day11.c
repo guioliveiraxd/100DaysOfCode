06126eeb1f1c5df817abf6df1ec36701fa2550b0
guioliveiraxd <guioliveira1177@gmail.com>
2026-06-20 23:20:40 -0300

    day11

--- Day11/day11.c
+++ Day11/day11.c
@@ -1,3 +1,30 @@
-//
-// Created by guiol on 20/06/2026.
-//
+#include <stdio.h>
+
+int main() {
+    int numero;
+    int soma = 0;
+    int positivos = 0;
+    float media;
+
+    do {
+        printf("Digite um numero: ");
+        scanf("%d", &numero);
+
+        if (numero > 0) {
+            soma += numero;
+            positivos++;
+        }
+
+    } while (numero != 0);
+
+    if (positivos > 0) {
+        media = (float)soma / positivos;
+
+        printf("\nSoma: %d\n", soma);
+        printf("Quantidade de positivos: %d\n", positivos);
+        printf("Media: %.2f\n", media);
+    } else {
+        printf("\nNenhum numero positivo foi digitado.\n");
+    }
+    return 0;
+}
