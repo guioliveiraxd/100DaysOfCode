67fd5416bc4445e56db061aa7281f25c6ce5d3c2
guioliveiraxd <guioliveira1177@gmail.com>
2026-06-23 21:34:58 -0300

    day14

---
+++ Day14/exemplo_for_matriz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[3][3];
+    int linha, coluna;
+
+    for (linha = 0; linha < 3; linha++) {
+
+        for (coluna = 0; coluna < 3; coluna++) {
+
+            printf("Digite um valor para [%d][%d]: ",
+                   linha, coluna);
+
+            scanf("%d", &matriz[linha][coluna]);
+        }
+    }
+    return 0;
+}
