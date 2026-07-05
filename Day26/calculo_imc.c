b16afc2fddca2647b8e69fdf5c9781f21f113614
guioliveiraxd <guioliveira1177@gmail.com>
2026-07-05 20:58:20 -0300

    day26

---
+++ Day26/calculo_imc.c
@@ -0,0 +1,3 @@
+//
+// Created by guiol on 05/07/2026.
+//
---
+++ Day26/varredura_vetor.c
@@ -0,0 +1,19 @@
+//
+// Simulado fatec maratona2025 / Exercício: Varredura de Vetor com Ponteiro
+//
+#include <stdio.h>
+
+int main() {
+    float vet[10] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+    float *p;
+    p = vet;
+
+    printf("Elementos do vetor:\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("%.1f\n", *p);
+        p++;
+    }
+
+    return 0;
+}
