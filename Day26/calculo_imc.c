5f5655716ced31cf33feebd53700dd434ec1a47f
guioliveiraxd <guioliveira1177@gmail.com>
2026-07-05 20:59:47 -0300

    day26

--- Day26/calculo_imc.c
+++ Day26/calculo_imc.c
@@ -1,3 +1,35 @@
 //
-// Created by guiol on 05/07/2026.
+// Simulado fatec maratona2025 / Exercício: calculo de imc
 //
+#include <stdio.h>
+
+int main() {
+    float peso;
+    float altura;
+    float imc;
+
+    printf("Digite o peso (kg): ");
+    scanf("%f", &peso);
+
+    printf("Digite a altura (m): ");
+    scanf("%f", &altura);
+
+    imc = peso / (altura * altura);
+
+    printf("\nIMC: %.2f\n", imc);
+
+    if (imc < 18.5) {
+        printf("Abaixo do peso\n");
+    }
+    else if (imc < 25.0) {
+        printf("Peso normal\n");
+    }
+    else if (imc < 30.0) {
+        printf("Sobrepeso\n");
+    }
+    else {
+        printf("Obesidade\n");
+    }
+
+    return 0;
+}
