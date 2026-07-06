9005681922ebdcc3af2db92710100b78e4c10e9e
guioliveiraxd <guioliveira1177@gmail.com>
2026-07-06 18:36:46 -0300

    day27

--- Day27/day27.c
+++ Day27/day27.c
@@ -1,3 +1,29 @@
 //
-// Created by guiol on 06/07/2026.
-//
+// Simulado fatec maratona2025 / Exercício: decisao do agricultor
+
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    double temperatura, umidade;
+    int chuva;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%lf %lf %d", &temperatura, &umidade, &chuva);
+
+        if(chuva == 1) {
+            printf("NAO REGAR\n");
+
+        } else {
+            if(temperatura > 30.0 && umidade < 50.0)
+                printf("REGAR\n");
+            else
+                printf("NAO REGAR\n");
+        }
+    }
+
+    return 0;
+}
