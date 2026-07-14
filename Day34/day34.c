82a1592e3afb44f7eacfebd4b414d2b41e36024f
guioliveiraxd <guioliveira1177@gmail.com>
2026-07-13 21:58:14 -0300

    dia 34

--- Day34/day34.c
+++ Day34/day34.c
@@ -1,3 +1,27 @@
 //
+//  Funcao para verificar se um numero e par
 //
-//
+#include <stdio.h>
+
+int ehPar(int numero) {
+    if (numero % 2 == 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main() {
+    int N;
+
+    printf("Digite um numero: ");
+    scanf("%d", &N);
+
+    if (ehPar(N)) {
+        printf("PAR\n");
+    } else {
+        printf("IMPAR\n");
+    }
+
+    return 0;
+}
