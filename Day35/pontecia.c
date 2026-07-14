6e6b99cf9c41270f9173820b5cd42d915737f71d
guioliveiraxd <guioliveira1177@gmail.com>
2026-07-14 19:02:39 -0300

    dia 35

---
+++ Day35/pontecia.c
@@ -0,0 +1,25 @@
+//
+//  Funcao para calcular potencia
+//
+#include <stdio.h>
+
+int potencia(int base, int expoente) {
+    int resultado = 1;
+
+    for (int i = 0; i < expoente; i++) {
+        resultado *= base;
+    }
+
+    return resultado;
+}
+
+int main() {
+    int base, expoente;
+
+    printf("Digite a base e o expoente: ");
+    scanf("%d %d", &base, &expoente);
+
+    printf("%d\n", potencia(base, expoente));
+
+    return 0;
+}
