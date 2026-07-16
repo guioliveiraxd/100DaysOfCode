952382c588ec61e38c8b990db0b1b2d6cb86dc5a
guioliveiraxd <guioliveira1177@gmail.com>
2026-07-16 18:55:09 -0300

    dia 37

--- Day37/day37.c
+++ Day37/day37.c
@@ -1,3 +1,38 @@
 //
-// Created by guiol on 16/07/2026.
+// mascarando senhas em c
 //
+#include <stdio.h>
+#include <string.h>
+
+void mascarar_senha(char *frase) {
+
+    int tamanho = strlen(frase);
+
+    if (frase[tamanho - 1] == '\n') {
+        frase[tamanho - 1] = '\0';
+        tamanho--;
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+
+        if (frase[i] == ' ') {
+            frase[i] = '-';
+        } else {
+            frase[i] = '*';
+        }
+    }
+
+    printf("\nSenha mascarada:\n%s\n", frase);
+}
+
+int main() {
+
+    char buffer[100];
+
+    printf("Digite a senha: ");
+    fgets(buffer, sizeof(buffer), stdin);
+
+    mascarar_senha(buffer);
+
+    return 0;
+}
