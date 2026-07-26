3f40bd36d3601ea2c6be1667deba882807f19570
guioliveiraxd <guioliveira1177@gmail.com>
2026-07-26 16:57:55 -0300

    dia 47

--- Day47/desafio_led.c
+++ Day47/desafio_led.c
@@ -1,3 +1,53 @@
 //
-// Created by guiol on 26/07/2026.
+//  Beecrowd 1168 - LED
 //
+#include <stdio.h>
+#include <string.h>
+
+int calcularLeds(char numero[]) {
+
+    int leds = 0;
+
+    for (int i = 0; i < strlen(numero); i++) {
+
+        if (numero[i] == '0') {
+            leds += 6;
+        } else if (numero[i] == '1') {
+            leds += 2;
+        } else if (numero[i] == '2') {
+            leds += 5;
+        } else if (numero[i] == '3') {
+            leds += 5;
+        } else if (numero[i] == '4') {
+            leds += 4;
+        } else if (numero[i] == '5') {
+            leds += 5;
+        } else if (numero[i] == '6') {
+            leds += 6;
+        } else if (numero[i] == '7') {
+            leds += 3;
+        } else if (numero[i] == '8') {
+            leds += 7;
+        } else if (numero[i] == '9') {
+            leds += 6;
+        }
+
+    }
+
+    return leds;
+}
+
+int main() {
+
+    char numero[101];
+    int total;
+
+    printf("Digite um numero: ");
+    scanf("%s", numero);
+
+    total = calcularLeds(numero);
+
+    printf("%d leds\n", total);
+
+    return 0;
+}
