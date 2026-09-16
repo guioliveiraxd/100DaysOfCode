ccd7a18705005c62e957adb94802e5b62e182884
Guilherme <guioliveira1177@gmail.com>
2026-09-16 20:27:34 -0300

    dia 97

---
+++ Day98/day98.c
@@ -0,0 +1,31 @@
+//
+//  Primeira letra que nao repete
+//
+#include <stdio.h>
+
+int main()
+{
+    char palavra[100];
+    int quantidade[256] = {0};
+
+    printf("Digite uma palavra: ");
+    scanf("%s", palavra);
+
+    for (int i = 0; palavra[i] != '\0'; i++)
+    {
+        quantidade[(unsigned char)palavra[i]]++;
+    }
+
+    for (int i = 0; palavra[i] != '\0'; i++)
+    {
+        if (quantidade[(unsigned char)palavra[i]] == 1)
+        {
+            printf("Primeira letra que nao repete: %c\n", palavra[i]);
+            return 0;
+        }
+    }
+
+    printf("Nenhuma letra encontrada.\n");
+
+    return 0;
+}
