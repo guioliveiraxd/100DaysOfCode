954b26cd41341f4c83e51ece8fb7a442254363aa
Guilherme Oliveira <164787576+guioliveiraxd@users.noreply.github.com>
2026-09-17 21:07:40 -0300

    Atualiza o day99.c

--- day99.c
+++ day99.c
@@ -1,1 +1,56 @@
-.
+//
+//  Compressao e Descompressao
+//
+#include <stdio.h>
+
+int main()
+{
+    char texto[100];
+    char comprimido[200];
+    char descomprimido[200];
+
+    int posicao = 0;
+    int posicaoDes = 0;
+
+    printf("Digite uma palavra: ");
+    scanf("%s", texto);
+
+    // Compressao
+    for (int i = 0; texto[i] != '\0'; i++)
+    {
+        int contador = 1;
+
+        while (texto[i] == texto[i + 1])
+        {
+            contador++;
+            i++;
+        }
+
+        comprimido[posicao++] = texto[i];
+
+        // Funciona para contagens de 1 a 9
+        comprimido[posicao++] = contador + '0';
+    }
+
+    comprimido[posicao] = '\0';
+
+    printf("\nComprimido: %s\n", comprimido);
+
+    // Descompressao
+    for (int i = 0; comprimido[i] != '\0'; i += 2)
+    {
+        char letra = comprimido[i];
+        int quantidade = comprimido[i + 1] - '0';
+
+        for (int j = 0; j < quantidade; j++)
+        {
+            descomprimido[posicaoDes++] = letra;
+        }
+    }
+
+    descomprimido[posicaoDes] = '\0';
+
+    printf("Descomprimido: %s\n", descomprimido);
+
+    return 0;
+}
